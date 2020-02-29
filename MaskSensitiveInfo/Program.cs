afbd593fd6a21fbb2ec18d277b1cabae11678e79
Crystal Broach <crysland@gmail.com>
2020-02-29 11:15:18 -0600

    Add project files.

---
+++ MaskSensitiveInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MaskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masking Sensitive Information");
+            Console.WriteLine("Enter a secret...");
+            var userSecret = Console.ReadLine();
+
+            if (userSecret.Length > 4)
+            {
+                var charsNumToCover = userSecret.Length - 4;
+                var maskedString = userSecret.Substring(charsNumToCover).PadLeft(charsNumToCover, '*');
+                Console.WriteLine(maskedString);
+            }
+            else
+            {
+                Console.WriteLine(userSecret);
+            }
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
